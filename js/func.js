dbed1bbaa3841d710943928a6f15ffedc13fab4a
flyketch <shifeiling@sina.com>
2019-08-31 20:11:41 +0800

    add fun

---
+++ js/func.js
@@ -0,0 +1,9 @@
+let address = '北京海淀区'
+let name = 'lala'
+let str = `${name}在${address}上班...`
+console.log(str)
+// 模拟一个方法 myTemplate(str) 最终输出 'lala在北京海淀区上班...'
+function myTemplate(str) {
+    // try it
+}
+console.log(myTemplate(str)) // lala在北京海淀区上班...
